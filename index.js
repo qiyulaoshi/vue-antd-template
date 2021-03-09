67d87d000d39baff3b7a8880801a7c6c88695a71
李秋水 <l>
2021-03-09 16:00:18 +0800

    3

--- index.js
+++ index.js
@@ -4,11 +4,10 @@
  * @Autor: 李秋水
  * @Date: 2021-03-09 15:41:39
  * @LastEditors: Seven
- * @LastEditTime: 2021-03-09 15:43:28
+ * @LastEditTime: 2021-03-09 16:00:11
  * @FilePath: \vue-antd-template\index.js
  */
 import CreateForm from './component/CreateForm'
-
 
 const ipaynow = {
     CreateForm
